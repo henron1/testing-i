3424931b2c496c33baba563a9642bf9446498526
Henry Neal <henry.neal0@gmail.com>
2019-03-04 15:49:47 -0500

    added fail success repair objects

---
+++ enhancer/enhancer.js
@@ -0,0 +1,19 @@
+module.exports = {
+    success,
+    failure,
+    repair,
+}
+
+function success(item) {
+
+}
+
+function failure() {
+
+
+}
+
+function repair() {
+
+    
+}
--- math/math.spec.js
+++ math/math.spec.js
@@ -1,4 +1,4 @@
-const math = require('./math');
+const {add} = require('./math');
 // SUT: System under test
 // teset suite > test cases > assertions > matchers
 // a test case
@@ -7,4 +7,8 @@ test('adds two numbers', () => {
     expect(math.add(2,2)).toBe(4);
     expect(math.add(1,3)).toBe(4);
     expect(math.add(-1, 3)).toBe(2);
-})
+});
+
+if('math.add() should throw and error when passed strings', () => {
+    expect(add)
+});
